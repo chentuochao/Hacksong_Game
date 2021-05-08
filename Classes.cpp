ac2c1eeab6c6809fe1658d37418a738e94f456a3
chentuochao <45381089+chentuochao@users.noreply.github.com>
2021-05-08 21:56:32 +0800

    finish the Classes.h and  Classes.cpp
    finish the Classes.h and  Classes.cpp

---
+++ Classes.cpp
@@ -0,0 +1,36 @@
+#include "Classes.h"
+#include "raylib.h"
+#include <iostream>
+
+using namespace std;
+
+Player::Player()
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+
+PKU_object::PKU_object(){
+
+}
+
+PKU_object::~PKU_object()
+{
+
+}
+
+
+PKU_event::PKU_event()
+{
+
+}
+
+PKU_event::~PKU_event()
+{
+    
+}
