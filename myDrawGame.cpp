c6924c3ced0dfcb8429faac5ecf4fd6d2bc3a52f
HYLZ-2019 <1657181739@qq.com>
2021-05-08 22:23:10 +0800

    frame of myDrawGame
    Only the function names.
    Test Github Desktop.

---
+++ myDrawGame.cpp
@@ -0,0 +1,39 @@
+#include "myDrawGame.h"
+
+void myDrawGame(){
+    if (!my_gameover){
+        myDrawBackground();
+        myDrawLines();
+        myDrawPlayers();
+        myDrawObjectsToPick();
+        myDrawEventPlace();
+        myDrawInfo();
+    }
+    else{
+        myDrawGameEnd();
+    }
+}
+
+void myDrawBackground(){
+
+}
+
+void myDrawLines(){
+
+}
+
+void myDrawPlayers(){
+
+}
+
+void myDrawObjectsToPick(){
+
+}
+
+void myDrawEventPlace(){
+
+}
+
+void myDrawInfo(){
+    
+}
